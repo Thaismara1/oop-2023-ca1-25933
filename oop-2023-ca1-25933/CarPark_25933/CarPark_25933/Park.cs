3dec41ced4a537094803f66ac9cc11850996e9b2
Thaismara1 <118394328+Thaismara1@users.noreply.github.com>
2023-11-27 12:06:36 +0000

    Add files via upload
    oop-2023-ca1-25933
    
    
    Abstract class is a type it serves as a base class for other classes to derive from. Abstract classes leave the implementation details up to the derived classes.
    
    when you have several classes connected to one class.
    ex public class Car: Vehicle
    vehicle is an Abstract class, where different types of transport are linked

---
+++ oop-2023-ca1-25933/CarPark_25933/CarPark_25933/Park.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop-2023-ca1-25933
+{
+    internal class Park
+    {
+        public String Customer { get; set; }
+        public int Garage { get; set; }
+        public double Hours { get; set; }
+        
+        public Park(int garage) 
+        { 
+            Garage = garage;
+        }   
+
+        public Park(int customer, int garage, double hours)
+            :this(garage)
+        {
+            this.Customer = customer;
+            this.Garage = garage;
+            this.Hours = hours;
+
+        }
+
+
+
+
+    }
+}
+
+
---
+++ oop-2023-ca1-25933/CarPark_25933/CarPark_25933/Program.cs
@@ -0,0 +1,101 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Imput Customer ID");
+        Console.WriteLine("in which garage it was parked? options 1, 2 and 3");
+        Console.WriteLine("How many hours of parking?");
+
+       
+
+        double charge;
+
+        Park park1 = new Park(1,1,6);
+
+        //var garage = new Park(1);
+
+
+        switch (new garege())
+        {
+            case "1":
+
+                if (Hours <= 3)
+                {
+                    Console.WriteLine($"your charge is €2.00");
+                }
+
+                else if (Hours > 3)
+                {
+                    for (int i = 3; i > 3; i++)
+                    {
+                        charge += 0.50;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"your charge is €10.00");
+
+                }
+                switch (Garage())
+
+                {
+                case "2":
+
+                        if (Hours <= 3)
+                        {
+                            Console.WriteLine($"your charge is €2.00");
+                        }
+
+                        else if (Hours > 3)
+                        {
+                            for (int i = 3; i > 3; i++)
+                            {
+                                charge += 0.60;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"your charge is €10.00");
+
+                        }
+
+                        switch (Garage())
+
+                        {
+
+                            case "3":
+
+                                if (Hours <= 3)
+                                {
+                                    Console.WriteLine($"your charge is €2.00");
+                                }
+
+                                else if (Hours > 3)
+                                {
+                                    for (int i = 3; i > 3; i++)
+                                    {
+                                        charge += 0.75;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"your charge is €10.00");
+                                }
+
+
+
+
+
+
+
+                        }
+
+
+                }
+        }
+
+
+
+        
+    }
+}
